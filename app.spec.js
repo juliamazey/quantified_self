a32dfea41df98196e638838e7d8745a72debeabf
Chris Lewis <13chlewis@gmail.com>
2019-05-07 06:49:59 -0600

    Add and test GET single food endpoint

--- app.spec.js
+++ app.spec.js
@@ -4,24 +4,41 @@ var app = require('./app');
 
 describe('api', () => {
   beforeEach(() => {
-    shell.exec('npx sequelize db:migrate:undo:all')
-    shell.exec('npx sequelize db:create')
-    shell.exec('npx sequelize db:migrate')
-    shell.exec('npx sequelize db:seed:all')
-  })
+    shell.exec('npx sequelize db:migrate:undo:all');
+    shell.exec('npx sequelize db:create');
+    shell.exec('npx sequelize db:migrate');
+    shell.exec('npx sequelize db:seed:all');
+  });
 
   describe('Test /api/v1/foods path', () => {
     test('should return a 200 status', () => {
       return request(app).get('/api/v1/foods').then(response => {
         expect(response.status).toBe(200)
-      })
+      });
     });
+
     test('should return an array of foods', () => {
       return request(app).get('/api/v1/foods').then(response => {
         expect(response.body.length).toBe(6),
         expect(Object.keys(response.body[0])).toContain('name'),
         expect(Object.keys(response.body[0])).toContain('calories')
-      })
+      });
     });
   })
-})
+
+  describe('Test /api/v1/foods/:id path', () => {
+    test('should return a 200 status', () => {
+      return request(app).get('/api/v1/foods/1').then(response => {
+        expect(response.status).toBe(200)
+      });
+    });
+
+    test('should return a food object by id', () => {
+      return request(app).get('/api/v1/foods/1').then(response => {
+        expect(Object.keys(response.body)).toContain('name'),
+        expect(Object.keys(response.body)).toContain('calories'),
+        expect((response.body.id)).toBe(1)
+      });
+    });
+  })
+});
