e63bea2ecbf9ea660534bca9b8c10045b27d03d6
Chris Lewis <13chlewis@gmail.com>
2019-05-08 13:30:31 -0600

    Fix and fully test POST food to meal endpoint

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var Food = require('../../../models').Food;
 var Meal = require('../../../models').Meal;
+var MealFood = require('../../../models').MealFood;
 pry = require('pryjs')
 
 // GET meal by id
@@ -26,6 +27,25 @@ router.get("/:id", function(req, res) {
   .catch(error => {
     res.setHeader("Content-Type", "application/json");
     res.status(400).send({ error })
+  });
+});
+
+// POST food to meal
+router.post("/:meal_id/foods/:id", function(req, res) {
+  MealFood.findOrCreate({
+    where: {
+      MealId: req.params.meal_id,
+      FoodId: req.params.id
+    },
+    include: [ { model: Food, attributes: ['name'] }, { model: Meal, attributes: ['name'] } ]
+  })
+  .then(MealFood => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(201).send(JSON.stringify({ message: `Successfully added ${MealFood[0].Food.name} to ${MealFood[0].Meal.name}`}));
+  })
+  .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(404).send(JSON.stringify({ message: 'Meal or food does not exist in database'}));
   });
 });
 
