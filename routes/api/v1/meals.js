cb2f89f53cd0647ad5572611e884c49b6a8b4159
Chris Lewis <13chlewis@gmail.com>
2019-05-08 14:39:31 -0600

    Fix route for getting single meal

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -1,36 +1,12 @@
 var express = require("express");
 var router = express.Router();
 var Food = require('../../../models').Food;
-var MealFood = require('../../../models').MealFood;
 var Meal = require('../../../models').Meal;
 var MealFood = require('../../../models').MealFood;
 pry = require('pryjs')
 
-// GET all meals
-router.get("/", function(req, res) {
-  Meal.findAll({
-    attributes: ['id', 'name'],
-    include: { model: Food, attributes: ['id', 'name', 'calories'], through: {attributes: []}}
-  })
-    .then(meals => {
-      if (meals.length > 0) {
-        res.setHeader("Content-Type", "application/json");
-        res.status(200).send(JSON.stringify(meals));
-      }
-      else {
-        res.setHeader("Content-Type", "application/json");
-        res.status(404).send(JSON.stringify({ message: 'No meals in database'}));
-      }
-    })
-    .catch(error => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(400).send({ error })
-    });
-});
-
-
 // GET meal by id
-router.get("/:id", function(req, res) {
+router.get("/:id/foods", function(req, res) {
   Meal.findOne({
     where: {
       id: req.params.id,
