1fc72a1d0fa1808026de095bad768d513d911525
Chris Lewis <13chlewis@gmail.com>
2019-05-15 17:42:03 -0600

    Remove hardcoded ids from seeds files

--- seeders/20190508123613-meal_seed.js
+++ seeders/20190508123613-meal_seed.js
@@ -4,19 +4,16 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
 
     return queryInterface.bulkInsert('Meals', [{
-          id: 1,
           name: "Fruit Salad",
           createdAt: new Date(),
           updatedAt: new Date()
       },
       {
-          id: 2,
           name: "Lunch",
       	  createdAt: new Date(),
           updatedAt: new Date()
       },
       {
-          id: 3,
           name: "Dinner",
           createdAt: new Date(),
           updatedAt: new Date()
